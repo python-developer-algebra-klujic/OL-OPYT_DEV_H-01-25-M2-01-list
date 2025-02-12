a3e9cb71297719af744672a0d969f38ce269b593
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-12 19:28:35 +0100

    Update main_02.py

--- main_02.py
+++ main_02.py
@@ -1,10 +1,29 @@
 numbers = []
 
-for i in range(100):
-    numbers.append(i + 1)
+for i in range(10):
+    numbers.append(i)
+
+print(numbers[5])
+print()
+
+numbers_length = len(numbers)
+print('Duzina liste', numbers_length)
+print()
 
 
 # Krivi nacin ispisa liste
 # print(numbers)
 for number in numbers:
     print(number)
+print()
+
+numbers.insert(3, 'petsto')
+print(numbers[5])
+
+numbers_length = len(numbers)
+print('Duzina liste', numbers_length)
+print()
+
+for number in numbers:
+    print(number)
+print()
