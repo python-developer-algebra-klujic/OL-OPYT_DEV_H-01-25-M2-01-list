c417b8a2d2b5c3f5f35feb3762038865b173b050
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-12 19:12:40 +0100

    Create main_01.py

---
+++ main_01.py
@@ -0,0 +1,14 @@
+
+# indeksi  0  1  2
+numbers = [1, 2, 3]
+
+first_number = numbers[0]
+print(first_number)
+
+second_number = numbers[1]
+print(second_number)
+
+numbers.append(4)
+
+fourth_number = numbers[3]
+print(fourth_number)
